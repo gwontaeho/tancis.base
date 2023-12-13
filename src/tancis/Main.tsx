09504151242003a0716d8b60fc6696704eeb04a3
unknown <taeho8199@gmail.com>
2023-12-13 14:02:28 +0900

    ;

---
+++ src/tancis/Main.tsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+import { MainLayout } from "@/com/layouts/MainLayout";
+
+export const Main = () => {
+    return (
+        <MainLayout>
+            <Routes></Routes>
+        </MainLayout>
+    );
+};
