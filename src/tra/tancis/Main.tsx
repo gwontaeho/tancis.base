e7ea7ab33a4a373f87d07e52a403f662eb93df71
unknown <taeho8199@gmail.com>
2023-12-18 10:02:31 +0900

    ;

--- src/tra/tancis/Main.tsx
+++ src/tra/tancis/Main.tsx
@@ -1,10 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { MainLayout } from "@/comn/layouts/MainLayout";
 
 export const Main = () => {
-    return (
-        <MainLayout>
-            <Routes></Routes>
-        </MainLayout>
-    );
+    return <Routes></Routes>;
 };
