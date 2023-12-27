4cc25f4dcb582da33e2547150c060e6b9d2322d7
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2023-12-27 16:58:35 +0900

    ;

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Route } from 'react-router-dom'
-import { Base } from '@/comn'
-import { Main } from '@/tra/tancis/Main'
+import { Route } from "react-router-dom";
+import { Base } from "@/comn";
+import { Main } from "@/tra/tancis/Main";
+import { useEffect } from "react";
 
 function App() {
+    useEffect(() => {}, []);
+
     return (
         <Base>
             <Route path="*" element={<Main />} />
         </Base>
-    )
+    );
 }
 
-export default App
+export default App;
