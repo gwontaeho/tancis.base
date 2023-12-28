ef0e06eda2d4f0786a050f44a87db7948c102722
gwontaeho <75310299+gwontaeho@users.noreply.github.com>
2023-12-28 15:21:45 +0900

    ;

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,9 @@ import { Main } from "@/tra/tancis/Main";
 import { useEffect } from "react";
 
 function App() {
-    useEffect(() => {
-        // if (!indexedDB) return;
+    useEffect(() => {}, []);
 
-        test2();
-    }, []);
-
-    const test1 = () => {
-        const request = indexedDB.open("TANCIS");
-
-        /**
-         * create object store
-         */
-        request.onupgradeneeded = (event) => {
-            console.log("db created");
-            const db = request.result;
-            db.createObjectStore("COMN_CD", { keyPath: "key" });
-            console.log("store created");
-        };
-
-        /**
-         * db connected
-         */
-        request.onsuccess = (event) => {
-            console.log("db connected");
-            const db = request.result;
-            const ts = db.transaction("COMN_CD", "readwrite");
-            const os = ts.objectStore("COMN_CD");
-            os.add({ key: "test", code: [{ a: "a" }, { a: "b" }, { a: "c" }] });
-        };
-    };
-
-    const test2 = () => {
-        const request = indexedDB.open("TANCIS");
-
-        /**
-         * db connected
-         */
-        request.onsuccess = (event) => {
-            console.log("db connected");
-        };
-    };
+    const test1 = async () => {};
 
     return (
         <Base>
