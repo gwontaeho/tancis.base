c10a0c43cb1096d74d92797f7368d397f99df115
claud-bae <claud6998@gmail.com>
2024-01-04 09:14:26 +0900

    ;

--- src/tra/tancis/Routes.ts
+++ src/tra/tancis/Routes.ts
@@ -1,5 +1,11 @@
 export const TancisRoutes = [
     {
+        /*
+         * name : 메뉴 명
+         * base : 기본 경로
+         * to : 메뉴의 경로 (base + to로 주소 조합)
+         * children : 하위 경로 Array
+         */
         name: "Cargo",
         base: process.env.REACT_APP_BASE,
         children: [
